1949d61b3f509c8b345d4a40b606ebc748c7ae6f
vee-kodes <violachepchirchir23@gmail.com>
2025-11-18 18:05:19 +0300

    build: add frontend build and testing configuration

---
+++ frontend/cypress.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173', // Vite dev server default port
+    setupNodeEvents(on, config) {
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+  },
+})
